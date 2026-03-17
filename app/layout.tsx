87162f9798ea803298d7b495dcca07b2ca087e0c
bliss46 <ced.baumgarten@gmail.com>
2026-03-17 16:20:31 +0100

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Digital Integration",
+  description: "Solutions digitales sur mesure",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
