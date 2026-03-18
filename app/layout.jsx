3682147d367ff858f846fce5d47679e9d39ed89a
bliss46 <ced.baumgarten@gmail.com>
2026-03-18 16:58:37 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,13 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Digital Integration",
-  description: "Développement sur mesure, automatisation et IA appliquée",
+  description:
+    "Développement sur mesure, automatisation et intégration de solutions digitales pour les entreprises.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
-
-import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
